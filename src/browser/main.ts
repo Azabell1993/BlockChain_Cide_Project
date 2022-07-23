5424772f3e3e9562485388c44470352e76f214ff
Azabell1993 <moreirly@naver.com>
2022-07-23 20:24:10 +0900

    Modify index.html of CWASM Test

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -7,4 +7,11 @@ enum Status {
     MinelnProgress = "Mining a new Block..."
 }
 
-
+const amountEI = document.getElementById('amount') as HTMLInputElement;
+const blocksEl              = document.getElementById('blocks') as HTMLDivElement;
+const confirmBtn            = document.getElementById('confirm') as HTMLButtonElement;
+const pendingTransactionsEl = document.getElementById('pending-transactions') as HTMLPreElement;
+const recipientEl           = document.getElementById('recipient') as HTMLInputElement;
+const senderEl              = document.getElementById('sender') as HTMLInputElement;
+const statusEl              = document.getElementById('status') as HTMLParagraphElement;
+const transferBtn           = document.getElementById('transfer') as HTMLButtonElement;
